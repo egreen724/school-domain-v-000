62a134e2203956a77a0db62cc21c308ad6bf2702
Erin Green <erin.y.green@gmail.com>
2018-09-26 22:10:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+end 
+
+def name=(name)
+  @name = name
+end 
+
+def name 
+  @name
+end
+
+def roster=(roster)
+  @roster = roster
+end
+
+def roster 
+  @roster
+end   
+  
+  def add_student(student_name, grade)
+    @roster[grade] = [] 
+    @roster[grade] << student_name
+  end 
+  
+end 
